a2ac36a6c8ef89f0049f296052e688c495db10b6
Alexander <alez.grante@gmail.com>
2022-02-10 23:18:00 +0300

    removed 2 classes

--- app/src/main/java/com/example/jokeapp/data/TestDataSource.kt
+++ app/src/main/java/com/example/jokeapp/data/TestDataSource.kt
@@ -2,18 +2,3 @@ package com.example.jokeapp.data
 
 import com.example.jokeapp.models.Joke
 
-class TestDataSource : CacheDataSource {
-
-    private val map = HashMap<Int,JokeServerModel>()
-
-    override fun addOrRemove(id: Int, jokeServerModel: JokeServerModel): Joke {
-        return if(map.containsKey(id)){
-            val joke = map[id]!!.toBaseJoke()
-            map.remove(id)
-            joke
-        }else{
-            map[id] = jokeServerModel
-            jokeServerModel.toFavouriteJoke()
-        }
-    }
-}
